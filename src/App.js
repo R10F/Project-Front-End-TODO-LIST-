f6798914c3a7bd5f73893ecd9f6e12ab8d6675c3
R10F <farrellriofa@gmail.com>
2022-06-21 18:01:10 +0700

    hampir selesai tampilan

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import "./App.css";
 import React from "react";
-
+import Swal from "sweetalert2";
 class App extends React.Component {
   render() {
     return (
-      <div className="container-xxl">
-        <h1 className="text-center">TODO LIST</h1>
+      <div className="container">
+        <h1 className="text-center my-5">TODO LIST</h1>
 
-        <form>
-          <div className="mt-5 mb-3">
-            <input type="text" className="form-control border-primary border-3" />
+        <form className="">
+          <div className="mt-5 mb-3 input-group">
+            <input type="text" className="form-control border-primary border-3 " placeholder="Enter New Todo" />
+            <button className="btn btn-primary input-group-text" type="button" onClick={this.klik}>
+              Add Todo
+            </button>
           </div>
 
           <div className="form-check form-check-inline">
-            <input class="form-check-input" type="radio" />
+            <input class="form-check-input " type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
               Low
             </label>
           </div>
 
           <div className="form-check form-check-inline ">
-            <input class="form-check-input" type="radio" />
+            <input class="form-check-input" type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
               Medium
             </label>
           </div>
 
           <div className="form-check form-check-inline">
-            <input class="form-check-input" type="radio" />
+            <input class="form-check-input" type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
               High Priority
             </label>
