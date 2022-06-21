ba9505535171a6acbdf1ff8c65e26e2373eb3df9
R10F <farrellriofa@gmail.com>
2022-06-21 16:09:25 +0700

    membuat tampilan input & radio (belum siap)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import { SiAddthis } from "react-icons/si";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+      <div className="container-xxl">
+        <h1 className="text-center">TODO LIST</h1>
+
+        <form>
+          <div className="mt-5 mb-3">
+            <input type="text" className="form-control border-primary border-3" />
+          </div>
+
+          <div className="form-check form-check-inline">
+            <input class="form-check-input" type="radio" />
+            <label class="form-check-label" for="flexRadioDefault1">
+              Low
+            </label>
+          </div>
+
+          <div className="form-check form-check-inline ">
+            <input class="form-check-input" type="radio" />
+            <label class="form-check-label" for="flexRadioDefault1">
+              Medium
+            </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+            <input class="form-check-input" type="radio" />
+            <label class="form-check-label" for="flexRadioDefault1">
+              High Priority
+            </label>
+          </div>
+        </form>
       </div>
     );
   }
