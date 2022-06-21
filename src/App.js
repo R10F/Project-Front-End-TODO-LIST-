8fe08c800dbd81f1e4a06368d741c0223d36130d
R10F <farrellriofa@gmail.com>
2022-06-21 18:58:27 +0700

    membuat input & radio

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import "./App.css";
 import React from "react";
-
+import Swal from "sweetalert2";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Tanggal: new Date() };
+  }
+
   render() {
     return (
-      <div className="container">
-        <h1 className="text-center my-5">TODO LIST</h1>
+      <div className="container-fluid">
+        <header className="bg-info">
+          <h1>
+            TODO LIST <span>{this.state.Tanggal.toDateString()} </span>
+          </h1>
+        </header>
 
-        <form className="">
-          <div className="mt-5 mb-3 input-group">
+        <form>
+          <div className="input-group">
             <input type="text" className="form-control border-primary border-3 " placeholder="Enter New Todo" />
-            <button className="btn btn-primary input-group-text" type="button" onClick={this.klik}>
+            <button className="btn btn-primary input-group-text" type="button">
               Add Todo
             </button>
           </div>
 
-          <div className="form-check form-check-inline">
+          <div className="form-check form-check-inline ">
             <input class="form-check-input " type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
-              Low
+              Low Priority
             </label>
           </div>
 
           <div className="form-check form-check-inline ">
             <input class="form-check-input" type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
-              Medium
+              Medium Priority
             </label>
           </div>
 
-          <div className="form-check form-check-inline">
+          <div className="form-check form-check-inline ">
             <input class="form-check-input" type="radio" name="filter" />
             <label class="form-check-label" for="flexRadioDefault1">
               High Priority
