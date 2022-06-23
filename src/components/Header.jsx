8e8a4cad2ba4d8fed14ed42f7ff678e64e81b4cd
fdavidsen <f.davidsen19@gmail.com>
2022-06-23 22:24:55 +0700

    Fix: Merging error\n.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,9 @@ class Header extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <header className="d-flex align-items-center justify-content-between sticky-top py-3 px-5">
         <h1 >TODO LIST</h1>
         <h1 className="date">{ this.state.Tanggal.toLocaleString("id-ID", { day: "numeric", month: "long", year: "numeric" }) }</h1>
-=======
-      <header className="d-flex align-items-center justify-content-between">
-        <h1>TODO LIST</h1>
-        <h1 className="date">{this.state.Tanggal.toLocaleString("id-ID", { day: "numeric", month: "long", year: "numeric" })}</h1>
->>>>>>> cd64f6123940f2bd8979db92fe08a18186987aca
       </header>
     );
   }
