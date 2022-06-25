7191b6232cd2c34ddfb5cc9ca61c5bda77e38cdc
eringnwn <105049956+eringnwn@users.noreply.github.com>
2022-06-25 22:52:11 +0700

    add: link; animation
    sidebar nav can now link to other endpoints (/, /in-progress, and /completed) and 'active' when it is clicked

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 import { FiList, FiClock, FiCheckCircle } from "react-icons/fi";
+import { Link, NavLink } from "react-router-dom";
 
 class Sidebar extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      activedList : "all"
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e){
+    console.log("e " + e.target.value);
+    this.setState({activedList: e.target.value});
+    console.log(this.state.activedList);
+  }
+  active = (key) => {
+    if (key === this.state.activedList) return true;
+    return false;
+  }
   render() {
+    const activeClassName = "nav-link py-3 border-bottom active";
+    const notactiveClassName = "nav-link py-3 border-bottom";
     return (
-      <aside className="d-flex flex-column align-items-center justify-content-between shadow position-fixed top-0 mt-5">
-        <div className="nav nav-pills nav-flush flex-column text-center mt-lg-4">
-          <button className="nav-link active pl-3 py-3 border-bottom" title="All">
-            <FiList /> <br/> 
-            <span className="d-none d-md-inline text-truncate">All</span>
-          </button>
-          <button className="nav-link py-3 border-bottom" title="In Progress">
-            <FiClock /> <br/>
-            {/* In Progress */}
-            <span className="d-none d-md-inline text-truncate">In Progress</span>
-          </button>
-          <button className="nav-link py-3 border-bottom" title="Completed">
-            <FiCheckCircle /> <br/>
-            {/* Completed */}
-            <span className="d-none d-md-inline text-truncate">Completed</span>
-          </button>
-        </div>
-        
-        <button className="btn btn-primary w-75 mb-4">Reset</button>
+      <aside className="shadow position-fixed top-0 mt-5">
+          <ul className="nav nav-pills flex-column text-center mt-lg-4">
+              <li className="nav-item" onClick={this.handleChange} value="all">
+                <NavLink to="/" className={({ isActive }) => isActive ? activeClassName : notactiveClassName}>
+                  <FiList /> <br/> 
+                  <span className="d-none d-md-inline text-truncate">All</span>
+                </NavLink>
+              </li>
+              <li className="nav-item" onClick={this.handleChange} value="inprogress">
+                <NavLink to="/in-progress" className={({ isActive }) => isActive ? activeClassName : notactiveClassName}>
+                  <FiClock /> <br/>
+                  <span className="d-none d-md-inline text-truncate">In Progress</span>
+                </NavLink>
+              </li>
+              <li className="nav-item" onClick={this.handleChange} value="completed">
+                <NavLink to="/completed"className={({ isActive }) => isActive ? activeClassName : notactiveClassName}>
+                  <FiCheckCircle /> <br/>
+                  <span className="d-none d-md-inline text-truncate">Completed</span>
+                </NavLink>
+              </li>
+          </ul>
       </aside>
     );
   }
