4158338b909769d903d153168aac59ef8d12c59e
eringnwn <105049956+eringnwn@users.noreply.github.com>
2022-06-25 22:44:16 +0700

    Update App.js
    routing

--- src/App.js
+++ src/App.js
@@ -1,64 +1,19 @@
 import "./App.css";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import AddTask from "./components/AddTask";
-import ListItem from "./components/ListItem";
+import Layout from "./layout/Layout";
+import Tasks from "./components/Tasks"
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { taskList: [] };
-  }
-
-  addTask = (newTask) => {
-    this.setState((state) => {
-      return { taskList: [...state.taskList, newTask] };
-    });
-  };
-
-  editTask = (id, editedTask) => {
-    this.setState((state) => {
-      state.taskList[id] = editedTask;
-      return { taskList: state.taskList };
-    });
-  };
-
-  deleteTask = (id) => {
-    this.state.taskList.splice(id, 1);
-    this.setState(state => {
-      return { taskList: state.taskList }
-    });
-  };
-
   render() {
     return (
-      <div className="container-fluid">
-        <Header />
-
-        <div className="d-flex">
-          <Sidebar />
-          
-          <div className="w-100 content">
-            <AddTask addTask={this.addTask} />
-
-            <div className="card m-5 p-2 ">
-              <ul className="list-group list-group-flush">
-                {this.state.taskList.map((item, index) => {
-                  return (
-                    <ListItem
-                      id={index}
-                      taskDetails={item}
-                      editTask={this.editTask}
-                      deleteTask={this.deleteTask}
-                    />
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={ <Layout />}>
+          <Route index element={ <Tasks filter={"All"}/> }/>
+          <Route path="in-progress" element={ < Tasks filter={"InProgress"}/> } />
+          <Route path="completed" element={ <Tasks filter={"Completed"} />} />
+        </Route>
+      </Routes>
     );
   }
 }
