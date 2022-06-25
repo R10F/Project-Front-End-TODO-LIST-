1dd6198f69484223ad09e853dbb75e17ca51bc26
eringnwn <105049956+eringnwn@users.noreply.github.com>
2022-06-25 22:47:15 +0700

    Update AddTask.jsx
    added 'Done' button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,21 +4,22 @@ class AddTask extends React.Component {
   addTask = (e) => {
     e.preventDefault();
 
-    const task     = e.target.querySelector('input').value;
-    const priority = e.target.querySelector('input:checked').value;
+    const task = e.target.querySelector("input").value;
+    const priority = e.target.querySelector("input:checked").value;
+    const completed = false;
 
-    this.props.addTask([ task, priority ]);
-  }
-  
+    this.props.addTask([task, priority, completed]);
+  };
   render() {
     return (
-      <form onSubmit={this.addTask} className="add-task m-5 p-4">
+      <form onSubmit={this.addTask} className="m-5 p-4">
         <div className="input-group mb-3">
           <input type="text" className="form-control border-primary border-3 " placeholder="Enter New Todo" />
           <button className="btn btn-primary input-group-text" type="submit">
             Add Task
           </button>
         </div>
+
         <div className="form-check form-check-inline">
           <input className="form-check-input" id="lowPriority" type="radio" value="Low" name="priority" defaultChecked />
           <label className="form-check-label" htmlFor="lowPriority">
