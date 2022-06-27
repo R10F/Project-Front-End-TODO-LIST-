6ff4d095f08c30eb443d9f38d3e022f47f9c6be7
R10F <farrellriofa@gmail.com>
2022-06-27 18:45:54 +0700

    mengubah font menjadi lebih cantik

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
@@ -7,12 +9,12 @@ class Header extends React.Component {
 
   render() {
     return (
-      <header className="d-flex align-items-center  sticky-top py-3 px-5 m-0">
+      <header className="d-flex align-items-center  sticky-top py-3 px-5">
     
-        <img src={require("././img/todo.png")} alt="icon" style={{width : "30px", height : "30px"}}  /> 
-        <h1 className="fs-2 ms-3">TO-DO LIST</h1>
+        <img src={require("././img/todo.png")} alt="icon"   /> 
+        <h1 className="fs-2 ms-3 py-1 ">TO-DO LIST</h1>
        
-        <h1 className="date ms-auto">{this.state.Tanggal.toLocaleString("id-ID", { day: "numeric", month: "long", year: "numeric" })}</h1>
+        <h1 className="date ms-auto fs-2" >{this.state.Tanggal.toLocaleString("id-ID", { day: "numeric", month: "long", year: "numeric" })}</h1>
       </header>
     );
   }
