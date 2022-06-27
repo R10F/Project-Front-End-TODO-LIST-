82205e518b77ea872083ac90c4c2877721bf50ab
fdavidsen <f.davidsen19@gmail.com>
2022-06-27 14:44:33 +0700

    Update: Fix reset task; Improvement: Remove unnecessary code; Remove: index.html.bak

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,31 +4,17 @@ import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 class Layout extends React.Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <div className="container-fluid">
-                <Header />
-                <div className="d-flex">
-                    <Sidebar resetTask={this.props.resetTask} />
-                    <Outlet />
-                </div>
-            </div>
-        );
-    }
-=======
   render() {
     return (
       <div className="container-fluid">
         <Header />
         <div className="d-flex">
-          <Sidebar />
+          <Sidebar resetTask={this.props.resetTask} />
           <Outlet />
         </div>
       </div>
     );
   }
->>>>>>> 546bfa9614c4534cfa5466f019e3aeb4e1f4a547
 }
 
 export default Layout;
